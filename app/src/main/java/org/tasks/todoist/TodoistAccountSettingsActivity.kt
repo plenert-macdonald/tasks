2b220e1c078605c3b5db910ee50e38f6d53cf015
Aidan Macdonald <software@apmac.us>
2025-12-27 09:35:14 -0800

    subscribe banner gone

--- app/src/main/java/org/tasks/todoist/TodoistAccountSettingsActivity.kt
+++ app/src/main/java/org/tasks/todoist/TodoistAccountSettingsActivity.kt
@@ -1,18 +1,44 @@
 package org.tasks.todoist
 
 import android.app.Activity
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
+import androidx.appcompat.content.res.AppCompatResources
 import androidx.appcompat.widget.Toolbar
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updateLayoutParams
+import androidx.core.view.updatePadding
+import androidx.core.widget.addTextChangedListener
+import com.google.android.material.snackbar.BaseTransientBottomBar
 import org.tasks.data.entity.Task
 import org.tasks.data.UUIDHelper
 import dagger.hilt.android.AndroidEntryPoint
 import org.tasks.R
+import org.tasks.Strings.isNullOrEmpty
 import org.tasks.analytics.Constants
+import org.tasks.billing.PurchaseActivity
 import org.tasks.caldav.BaseCaldavAccountSettingsActivity
+import org.tasks.compose.ServerSelector
 import org.tasks.data.entity.CaldavAccount
+import org.tasks.data.entity.CaldavAccount.Companion.SERVER_UNKNOWN
+import org.tasks.data.entity.CaldavAccount.Companion.TYPE_LOCAL
 import org.tasks.data.getPassword
+import org.tasks.databinding.ActivityCaldavAccountSettingsBinding
+import org.tasks.dialogs.Linkify
+import org.tasks.extensions.addBackPressedCallback
+import org.tasks.themes.TasksTheme
 import timber.log.Timber
 import javax.inject.Inject
 
@@ -25,6 +51,75 @@ class TodoistAccountSettingsActivity : BaseCaldavAccountSettingsActivity(), Tool
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityCaldavAccountSettingsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { _, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            binding.toolbar.toolbar.updateLayoutParams<ViewGroup.MarginLayoutParams> {
+                topMargin = systemBars.top
+            }
+            binding.rootLayout.updatePadding(bottom = systemBars.bottom)
+            insets
+        }
+        caldavAccount = if (savedInstanceState == null) intent.getParcelableExtra(EXTRA_CALDAV_DATA) else savedInstanceState.getParcelable(EXTRA_CALDAV_DATA)
+        serverType = mutableStateOf(
+            savedInstanceState?.getInt(EXTRA_SERVER_TYPE, SERVER_UNKNOWN)
+                ?: caldavAccount?.serverType
+                ?: SERVER_UNKNOWN
+        )
+        if (caldavAccount == null || caldavAccount!!.id == Task.NO_ID) {
+            binding.nameLayout.visibility = View.GONE
+            binding.description.visibility = View.VISIBLE
+            binding.description.setText(description)
+            Linkify.safeLinkify(binding.description, android.text.util.Linkify.WEB_URLS)
+            serverType.value = SERVER_UNKNOWN
+        } else {
+            binding.nameLayout.visibility = View.VISIBLE
+            binding.description.visibility = View.GONE
+            caldavAccount?.error?.takeIf { it.isNotBlank() }?.let {
+                binding.description.visibility = View.VISIBLE
+                binding.description.setTextColor(ContextCompat.getColor(this, R.color.overdue))
+                binding.description.text = getString(R.string.error_adding_account, it)
+            }
+        }
+        if (savedInstanceState == null) {
+            caldavAccount?.let {
+                if (!isNullOrEmpty(it.password)) {
+                    binding.password.setText(PASSWORD_MASK)
+                }
+            }
+        }
+        val toolbar = binding.toolbar.toolbar
+        toolbar.title = if (caldavAccount == null) getString(R.string.add_account) else caldavAccount!!.name
+        toolbar.navigationIcon = AppCompatResources.getDrawable(this, R.drawable.ic_outline_save_24px)
+        toolbar.setNavigationOnClickListener { save() }
+        toolbar.inflateMenu(menuRes)
+        toolbar.setOnMenuItemClickListener(this)
+        toolbar.showOverflowMenu()
+        if (caldavAccount == null) {
+            toolbar.menu.findItem(R.id.remove).isVisible = false
+            binding.name.requestFocus()
+            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.showSoftInput(binding.name, InputMethodManager.SHOW_IMPLICIT)
+        }
+        binding.password.addTextChangedListener(
+            onTextChanged = { _, _, _, _ -> binding.passwordLayout.error = null }
+        )
+        binding.serverSelector.setContent {
+            TasksTheme(
+                theme = tasksTheme.themeBase.index,
+                primary = tasksTheme.themeColor.primaryColor,
+            ) {
+                var selected by rememberSaveable { serverType }
+                ServerSelector(selected) {
+                    serverType.value = it
+                    selected = it
+                }
+            }
+        }
+
         binding.serverSelector.visibility = View.GONE
         binding.showAdvanced.visibility = View.GONE
         binding.urlLayout.visibility = View.GONE
